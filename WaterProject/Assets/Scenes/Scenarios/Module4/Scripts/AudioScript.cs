2e7ff6f8b1a5891fd9f42f42acdcbea87311c75b
CobraKai1117 <43760180+CobraKai1117@users.noreply.github.com>
2019-03-25 15:29:41 -0500

    Interface Changes
    Organized scripts and audio by its module

---
+++ WaterProject/Assets/Scenes/Scenarios/Module4/Scripts/AudioScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+   public List<AudioClip> Tracks;
+    [SerializeField] int trackCount;
+    [SerializeField] Camera ARCam;
+    AudioSource Listen;
+ 
+    [SerializeField] GameObject Clipboard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trackCount = 0;
+        ARCam.gameObject.AddComponent<AudioSource>();
+        Listen = ARCam.GetComponent<AudioSource>();
+        Listen.playOnAwake = false;
+       
+
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        
+    }
+
+    private void Awake()
+    {
+       // Listen.playOnAwake = false;
+    }
+
+
+
+    public void PlayAudio()
+    {
+        Listen.clip = Tracks[trackCount];
+
+        Listen.Play();
+
+        trackCount++;
+    }
+
+    public void PauseAudio()
+    {
+        Listen.Pause();
+    }
+
+    public void UnpauseAudio()
+    {
+        Listen.UnPause();
+    }
+
+   
+
+}
---
+++ WaterProject/Assets/Scenes/Scenarios/Module4/Scripts/LabelKeeper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LabelKeeper : MonoBehaviour {
+
+    public GameObject LevelLabel;
+    public GameObject PauseLabel;
+
+    public string levelName;
+	// Use this for initialization
+	void Start () {
+        LevelLabel.GetComponentInChildren<Text>().text = levelName;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!LevelLabel.activeInHierarchy&&!PauseLabel.activeInHierarchy)
+        {
+            LevelLabel.SetActive(true);
+        }
+
+        if (PauseLabel.activeInHierarchy)
+        {
+            LevelLabel.SetActive(false);
+        }
+	}
+}
